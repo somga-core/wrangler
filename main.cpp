4061a4817341e1436da468ac00ff1dddac09e128
d54267ygi <d54267ygi@gmail.com>
2025-12-29 23:53:33 +0200

    An atempt to bring up a new structure, a lot of discussions over it are ahead

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "teco/teco_engine.hpp"
+#include "teco/teco_gui.hpp"
+#include "teco/teco_tui.hpp"
+
+#include <random>
+#include <ctime>
+
+#include "main_gameplay.cpp"
+
+int main() {
+	srand(time(0));
+
+	teco::init(&main_gameplay_screen, "Wrangler");
+	teco::init_gui();
+	teco::mainloop_gui();
+
+	return 0;
+}
+
---
+++ main_gameplay.cpp
@@ -0,0 +1,10 @@
+void main_gameplay_tick() {
+
+}
+
+void main_gameplay_draw() {
+
+}
+
+teco::Screen main_gameplay_screen(192, 56, &main_gameplay_tick, &main_gameplay_draw);
+
